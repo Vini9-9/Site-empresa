9dc5440f187fc76318c5e514113b4b28eab9e50e
Vinicius Pessoa <vini.pessoa99@gmail.com>
2020-06-17 00:14:26 -0300

    Criando página inicial do Site

---
+++ index.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+	<meta charset="UTF-8">
+	
+	<title> Confeitaria da Lili - Um mundo de doces </title>
+	
+	<meta name="description" content="Descrição do site em até 160 caracteres">
+	
+	<meta name="keywords" content="Palavras chave em uma busca rápida. Separar as palavras por vírgula.">
+	<meta name="author" content="Vinicius Pessoa ou Nome da Sua empresa">
+	<meta name="viewport" content="width=device-width, initial-scale=1">	
+	
+	<link rel="icon"  href="../images/favicon.ico">
+	<link rel="stylesheet" href="../libraries/stylesheet.css">
+	
+	<script src="../libraries/jquery-3.5.0.js"> </script>
+	<script src="../libraries/javascript.js"> </script>
+	 
+</head>
+
+<body>
+
+<header> 
+
+	 
+	<nav>
+		<ul>
+			<li><a href="index">Home</a></li>
+			<li><a href="sobrenos">Sobre Nós</a></li>
+			<li><a href="produtos">Produtos</a></li>
+			<li><a href="contato">Contato</a></li>
+			<li><a href="cadastrese">Cadastre-se</a></li>
+		</ul>
+	</nav>
+
+	<div class="ind-botao">
+    <img src="../images/botao.png" alt="Botão Mobile" title="Botão Mobile">
+</div>
+
+<div class="ind-logo">
+    <picture>
+        <!-- mobile  srcset="images/mobile-logo.png" -->
+		<source media="(max-width: 480px)" srcset="../images/logo.png">
+        <!-- tablet  srcset="images/mobile-logo.png" -->
+		<source media="(min-width: 481px) and (max-width: 768px)" srcset="../images/logo.png">
+        <!-- desktop  srcset="images/desktop-logo.jpg" -->
+		<source media="(min-width: 769px)" srcset="../images/logo.png"> 
+        <!-- backup -->
+		<img src="../images/mobile-logo.png" alt="Logomarca Arroba Gourmet" title="Logomarca Arroba Gourmet">
+    </picture>
+</div>
+	
+</header>
+	
+
+
+<main> 
+	
+	<!-- <h1> Bem vindo ao meu site! </h1> -->
+
+</main>
+
+
+
+<footer>
+
+<!-- contatos -->
+	
+</footer>
+	
+	
+</body>
+
+</html>
